8908dd50fa33782be7df71cc78fa018be817cfe8
Tikkumies <mikko_stick@hotmail.com>
2016-08-19 17:20:01 +0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,26 +1,26 @@
 <?php
+
+require_once("session.php");
+
 $dsn = 'mysql:host=127.0.0.1;dbname=kirjanpito';
 $username = 'root';
 $password = '';
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 ); 
-
 $dbh = new PDO($dsn, $username, $password, $options);
-
 $tuote = $_GET["tyyppi"];
 $hinta = $_GET["hinta"];
-
-$sql = "INSERT INTO kustannukset(tuoteryhma, hinta)
-    VALUES(:tyyppi, :hinta)";
+$date = date("Y-m-d");
+$sql = "INSERT INTO kustannukset(tuoteryhma, hinta, pvm)
+    VALUES(:tyyppi, :hinta, :pvm)";
 	
 $query = $dbh->prepare($sql);
 $results = $query->execute(array(
 	":tyyppi" => $tuote,
-	":hinta"=> $hinta
+	":hinta"=> $hinta,
+	":pvm"=> $date
 	));
 	
-echo "Tieto tallennettu";
-
-header("Location: http://localhost/kirjanpito/kustannukset.html")
+header("Location: http://localhost/kirjanpito/login/kustannukset.php")
 ?>
---
+++ kustannukset.php
@@ -0,0 +1,103 @@
+<?php
+
+	require_once("session.php");
+	
+	require_once("class.user.php");
+	$auth_user = new USER();
+	
+	
+	$user_id = $_SESSION['user_session'];
+	
+	$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+	$stmt->execute(array(":user_id"=>$user_id));
+	
+	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Kujujen seuranta</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Kulujen seuranta</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Koti</a></li>
+            <li><a href="#about">Historia</a></li>
+            <li><a href="#contact">Contact</a></li>
+			<li><a href="logout.php?logout=true">Kirjaudu ulos</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Syötä ostoksesi</h1>
+        <p class="lead">
+		
+
+		<form action="connect.php" method="get">
+			Tuotteen tyyppi:<br>
+				<input type="text" name="tyyppi"
+					><br><br>
+			Hinta:
+				<br>
+					<input type="text" name="hinta">
+				<br><br>
+
+					<input type="submit" value="Lähetä" class="btn btn-primary ">
+		</p>
+	
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
